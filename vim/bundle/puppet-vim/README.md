5365766e5eedcaa9f3558ea0c2e9931d0a0d4acc
Riley Spahn <riley@cs.columbia.edu>
2016-01-02 13:36:02 -0500

    Added nerdtree and removed puppet.

--- vim/bundle/puppet-vim/README.md
+++
@@ -1,2 +0,0 @@
-Manual clone of github.com/puppetlabs/puppet/ext/vim so that
-it can be used as a bundle with pathogen.
--- vim/bundle/puppet-vim/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Updates the contents of this repo with changes from upstream.
-
-wget -O - https://github.com/puppetlabs/puppet/tarball/master | tar xz --strip-components 3 --wildcards --no-anchored "*/ext/vim"
-
-if [ "$(git status --porcelain)" ]; then
-  git add . && git commit -am "Changes from upstream."
-fi
